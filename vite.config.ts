e5c828ffc3009222067c621a45fe840c4135ecfc
Leonardo Medina <leonarmed@globalbridgeconnections.com>
2023-06-21 21:37:20 -0400

    config githubpages

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,16 @@
 /* eslint-disable @typescript-eslint/triple-slash-reference */
 /// <reference types="vitest"/>
 /// <reference types="vite/client"/>
-import { defineConfig, splitVendorChunkPlugin } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, splitVendorChunkPlugin } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), splitVendorChunkPlugin()],
+  base: "https://leonarmed.github.io/agroinnova",
   test: {
     globals: true,
-    environment: 'jsdom',
-    setupFiles: ['./src/setupTests.ts'],
+    environment: "jsdom",
+    setupFiles: ["./src/setupTests.ts"],
   },
-})
+});
