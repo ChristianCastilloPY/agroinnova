c905e1953ececcbda3746b89154e7342dceb0795
Leonardo Medina <leonarmed@globalbridgeconnections.com>
2023-07-08 20:42:43 -0400

    Testing roots

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/agroinnova",
+  base: "/agroinnova/",
   plugins: [react(), splitVendorChunkPlugin()],
   // test: {
   //   globals: true,
